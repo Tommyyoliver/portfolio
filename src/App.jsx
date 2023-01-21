1c944b65b7a23ee9f1aca55dc712b034325646c5
Tommy Oliver <tommyolivr@gmail.com>
2023-01-20 21:34:37 -0300

    Sidebar agregada a la pagina inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import "./assets/css/output.css"
-import { InitialSeciton } from './components/InitialSection'
+import "./assets/css/output.css";
+import { Sidebar } from "./components/Sidebar";
+import { InitialSeciton } from './components/InitialSection';
 
 function App() {
   return (
     <div className="App">
+      <Sidebar />
       <InitialSeciton />
     </div>
   )
