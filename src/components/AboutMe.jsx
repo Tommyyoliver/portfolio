c49ed0f99b08935d39843b169718a2e8f0268b3b
Tommy Oliver <tommyolivr@gmail.com>
2023-01-25 14:56:56 -0300

    Sidebar actualizado. Mejorado de animaciones

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -18,9 +18,9 @@ function AboutMe () {
                 <h3>:) QUIÉN SOY</h3>
             </div>
             <div className="about-text-container">
-                <p>¡Hola! Mi nombre es <span>Tommy</span>, construyo paginas web. Soy un Desarrollador Front-End autodidacta iniciado en 2022 apasionado por este sector.</p>
+                <p>¡Hola! Mi nombre es <span>Tommy</span> y construyo páginas web. Soy un Desarrollador Front-End autodidacta iniciado en 2022.</p>
                 <p>Disfruto llevar a cabo la maquetación e interactividad de interfaces web, así como la optimización.</p>
-                <p>Mi <span>objetivo</span> ha sido el mismo desde un principio: obtener la atencion del usuario, más allá de un buen diseño, con una interfaz útil e interactiva.</p>
+                <p>Mi <span>objetivo</span> ha sido el mismo desde un principio: obtener la atención del usuario, más allá de un buen diseño, con una interfaz útil e interactiva.</p>
             </div>
             <div className="btn-container">
                 <div className="btn">
